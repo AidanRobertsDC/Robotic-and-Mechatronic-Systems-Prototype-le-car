ea74e7ad8581e07e1ddd4f67121a14663ddedf2b
AidanRobertsDC <141705812+AidanRobertsDC@users.noreply.github.com>
2024-05-02 22:33:03 +1000

    Update hough transform 2 lines.py

--- hough transform 2 lines.py
+++ hough transform 2 lines.py
@@ -30,10 +30,10 @@ while True:
                 vertical_lines.append(lines[i])
         similer_angle.append(verical_lines[0])
         for line in vertical_lines[1:]:
-            if((vertical_lines[0][0][1]/line[0][1]<2) and (vertical_lines[0][0][1]/line[0][1]>0))
+            if((vertical_lines[0][0][1]/line[0][1]<2) and (vertical_lines[0][0][1]/line[0][1]>0)):
                 similer_angle.append(line)
         for line in similer_angle[1:]:
-            if((similer_angle[0][0][0]/line[0][0]>2) and (similer_angle[0][0][0]/line[0][0]<0))
+            if((similer_angle[0][0][0]/line[0][0]>2) and (similer_angle[0][0][0]/line[0][0]<0)):
                 rho = line[0][0]
                 theta = line[0][1]  
                 a = math.cos(theta)

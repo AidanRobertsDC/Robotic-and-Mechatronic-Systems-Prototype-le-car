a9c302b357320d65fd13f733cbf5a56c8f643875
AidanRobertsDC <141705812+AidanRobertsDC@users.noreply.github.com>
2024-05-21 11:47:51 +1000

    Update driving.py

--- driving.py
+++ driving.py
@@ -5,6 +5,7 @@ from gpiozero import Robot
 
 cam = cv2.VideoCapture(0)
 robot = Robot((17,27), (22,23))
+key = cv2.waitKey(1)
 
 while True:
     check, frame = cam.read()
@@ -43,38 +44,37 @@ while True:
             if len(line) > 0:
                 if((vertical_lines[0][0][1]/line[0][1]<1.5) and (vertical_lines[0][0][1]/line[0][1]>0.5)):#Getting the theta of first line in vertical_lines and dividng by the theta of other lines in vertical_lines to see if they have similer enough theta.
                     similer_angle.append(line)
-        similer_angle.sort(key=seperation) 
-        first = similer_angle[0]  
-        last = similer_angle[-1]    
-        woking = [first, last]
-        for line in woking:        
-            rho = line[0][0]
-            theta = line[0][1]  
-            a = math.cos(theta)
-            b = math.sin(theta)
-            x0 = a * rho 
-            y0 = b * rho
-            pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
-            pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
-            cv2.line(image, pt1, pt2, (0,0,255), 3, cv2.LINE_AA) 
-            cv2.circle(image,(447,63), 63, (0,0,255), -1) 
-            cv2.imshow("Detected Lines (in red) - Standard Hough Line Transform", image) 
-        centerline = (first[0][0] + last[0][0])/2
-        center = image_width/2
-        if centerline - center > 0:
-            while centerline - center > 0:
-                robot.right()
+        if len(similer_angle) > 0:
+            similer_angle.sort(key=seperation) 
+            first = similer_angle[0]  
+            last = similer_angle[-1]    
+            woking = [first, last]
+            for line in woking:        
+                rho = line[0][0]
+                theta = line[0][1]  
+                a = math.cos(theta)
+                b = math.sin(theta)
+                x0 = a * rho 
+                y0 = b * rho
+                pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
+                pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
+                cv2.line(image, pt1, pt2, (0,0,255), 3, cv2.LINE_AA) 
+                cv2.circle(image,(447,63), 63, (0,0,255), -1) 
+                cv2.imshow("Detected Lines (in red) - Standard Hough Line Transform", image) 
+            centerline = (first[0][0] + last[0][0])/2
+            center = image_width/2
+            if centerline - center > 0:
+                while centerline - center > 0:
+                    robot.right()
+                robot.stop()
+            elif centerline - center < 0:
+                    while centerline - center < 0:
+                        robot.left()
             robot.stop()
-        elif centerline - center < 0:
-            while centerline - center < 0:
-                robot.left()
-            robot.stop()
-        else:
             while centerline - center == 0:
-                robot.forward()
+                    robot.forward()
             robot.stop()
-           
-    key = cv2.waitKey(1)
+        
     if key == 27:
         break
 cam.release()
